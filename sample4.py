41e9b986bf1ad5aadc3756a5f2a690a3ca963f1a
Liberbell <liberbell@gmail.com>
2021-01-26 09:03:20 +0900

    sample44

--- sample4.py
+++ sample4.py
@@ -4,5 +4,8 @@ name = 'ted'
 print(fmt.format(name))
 print(fmt.format('bob'))
 
-fmt = 'My name is {}{}.'
+fmt = 'My name is {} {}.'
 print(fmt.format('jake', 'Calen'))
+
+fmt = '{0} {1} {0}'
+print(fmt.format('a1', 'a2'))
