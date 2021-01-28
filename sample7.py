e9f3e6000fc9619907f60976694db66f2d04ca70
Liberbell <liberbell@gmail.com>
2021-01-29 08:41:31 +0900

    sample72

--- sample7.py
+++ sample7.py
@@ -6,3 +6,9 @@ my_tuple = (1, True, False, 'hello')
 print(type(my_tuple))
 my_tuple = 1, True, False, 'hello'
 print(type(my_tuple))
+print(my_tuple)
+
+one_tuple = 'hello'
+print(type(one_tuple))
+one_tuple = 'hello',
+print(type(one_tuple))
