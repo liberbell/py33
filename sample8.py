ce59cc8b56ab67e34ed53a97c2f0fed2e8cb1b07
Liberbell <liberbell@gmail.com>
2021-01-29 08:51:44 +0900

    sample85

--- sample8.py
+++ sample8.py
@@ -19,3 +19,9 @@ numbers1 = [1, 2, 3]
 numbers2 = [4, 5, 6]
 numbers3 = numbers1 + numbers2
 print(numbers3)
+
+numbers = [1, 2, 3, 4, 5]
+numbers.pop(0)
+print(numbers)
+numbers.pop()
+print(numbers)
