4e918c9980bc8631e2c965fe72a409619308ed77
Liberbell <liberbell@gmail.com>
2021-01-31 13:20:45 +0900

    sample1104

--- sample11.py
+++ sample11.py
@@ -8,5 +8,8 @@ my_set = set(my_list)
 print(my_set)
 print(type(my_set))
 
-my_set = [1, 2, 4, 4, 3, 5, 2, 8, 9, 1]
+my_list = [1, 2, 4, 4, 3, 5, 2, 8, 9, 1]
+print(my_set)
+
+my_set = set(my_list)
 print(my_set)
