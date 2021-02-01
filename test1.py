1c82c2d8ecc811f8e8dc11f410efd213a9ea622d
Liberbell <liberbell@gmail.com>
2021-02-02 08:44:41 +0900

    test14

--- test1.py
+++ test1.py
@@ -23,3 +23,9 @@ elif color == 'white':
     print('Color is white')
 else:
     print('Color is other color.')
+
+numbers = [1, 2, 3, 4, 5]
+if 1 in numbers and 2 in numbers:
+    print('1 and 2 are in the list.')
+else:
+    print('out of list number.')
