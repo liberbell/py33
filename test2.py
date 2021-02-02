67b0f8070d4de09f1522496e5c5a0a284d145cd0
Liberbell <liberbell@gmail.com>
2021-02-02 09:03:41 +0900

    test26

--- test2.py
+++ test2.py
@@ -13,6 +13,6 @@ for name in names:
 band_members = {'vocal': 'James', 'piano': 'Elton', 'guiter': 'Eric'}
 for part in band_members:
     print(part)
-    name = band_members(part)
+    name = band_members[part]
     result = '{0} is {1}'.format(part, name)
     print(result)
