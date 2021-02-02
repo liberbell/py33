5051b48181723a1ca204778c86b17aaf43beb733
Liberbell <liberbell@gmail.com>
2021-02-02 09:00:38 +0900

    test24

--- test2.py
+++ test2.py
@@ -6,6 +6,10 @@ for name in names:
 # for char in 'Helloworld':
 #     print(char)
 
-for name in names:
-    for char in name:
-        print(char)
+# for name in names:
+#     for char in name:
+#         print(char)
+
+band_members = ['vocal': 'James', 'piano': 'Elton', 'guiter': 'Eric']
+for part in band_members:
+    print(part)
