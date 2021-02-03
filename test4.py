4c7f4a7252e3ede5cc98eed13402f18f7636da9d
Liberbell <liberbell@gmail.com>
2021-02-04 08:47:15 +0900

    test42

--- test4.py
+++ test4.py
@@ -3,7 +3,7 @@ for i in range(30):
         print('fizz buzz')
     elif i % 5 == 0:
         print('buzz')
-    elif i % 3 = 0:
+    elif i % 3 == 0:
         print('fizz')
     else:
         print(i)
