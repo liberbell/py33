7ed6963a8bc9a9c0003a94f6e4ebc513e43774db
Liberbell <liberbell@gmail.com>
2021-02-05 08:57:09 +0900

    test73

--- test7.py
+++ test7.py
@@ -1,3 +1,7 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 squares = [num**2 for num in numbers]
 print(squares)
+
+words = ['python', 'django', 'tkinter']
+upper_words = [word.upper() for word in words]
+print(upper_words)
