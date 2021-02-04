224305b63a3b64504a4c55be9c56b870fb43508c
Liberbell <liberbell@gmail.com>
2021-02-05 08:43:40 +0900

    test61

--- test6.py
+++ test6.py
@@ -1,8 +1,10 @@
-while True:
-    user_input = input()
-    if user_input == 'exit':
-        break
-    elif user_input == 'skip':
-        continue
-    else:
-        print('input: ', user_input)
+# while True:
+#     user_input = input()
+#     if user_input == 'exit':
+#         break
+#     elif user_input == 'skip':
+#         continue
+#     else:
+#         print('input: ', user_input)
+
+names = ['JamesBrown', 'EltonJohn', 'JeffBeck']
