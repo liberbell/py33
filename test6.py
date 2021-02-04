0932f8bf029e7d1c026cc613f98d93ecd94c2511
Liberbell <liberbell@gmail.com>
2021-02-05 08:46:30 +0900

    test62

--- test6.py
+++ test6.py
@@ -8,3 +8,12 @@
 #         print('input: ', user_input)
 
 names = ['JamesBrown', 'EltonJohn', 'JeffBeck']
+is_found = False
+for name in names:
+    if name.endswith('John'):
+        is_found = True
+
+if is_found == True:
+    print('We found!')
+else:
+    print('no body there.')
