b84bbff13ee011bd4737645e897831406110ba33
Liberbell <liberbell@gmail.com>
2021-02-04 09:07:49 +0900

    test53

--- test5.py
+++ test5.py
@@ -1,4 +1,10 @@
-count = 0
-while count < 10:
-    print(count)
-    count += 1
+# count = 0
+# while count < 10:
+#     print(count)
+#     count += 1
+
+flag = True
+while flag:
+    user_input = input()
+    if user_input == 'exit':
+        flag = False
