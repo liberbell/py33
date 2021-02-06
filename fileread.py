49f972d9559e9a3deb59f84f37bbba8b43f6b0cc
Liberbell <liberbell@gmail.com>
2021-02-06 21:25:05 +0900

    fileread2

--- fileread.py
+++ fileread.py
@@ -1,3 +1,8 @@
 file = open('hello.txt', 'r', encoding='utf-8')
 src = file.read()
 print(src)
+file.close()
+
+with open('hello.txt', 'r', encoding='utf-8') as file:
+    src = file.read()
+    print(src)
