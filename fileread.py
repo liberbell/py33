12518b69a086b20c6c135d4404c54d36020f0050
Liberbell <liberbell@gmail.com>
2021-02-07 12:39:10 +0900

    fileread3

--- fileread.py
+++ fileread.py
@@ -7,7 +7,19 @@
 #     src = file.read()
 #     print(src)
 
-with open('hello.txt', 'r', encoding='utf-8') as file:
+# with open('hello.txt', 'r', encoding='utf-8') as file:
+#     for line in file:
+#         print(line, end='')
+#         print(type(line))
+
+text = '''Hello
+Goody
+Eveny'''
+
+with open('hello3.txt', 'w', encoding='cp932') as file:
+    file.write()
+
+with open('hello3.txt', 'r', encoding='utf-8') as file:
     for line in file:
         print(line, end='')
         print(type(line))
