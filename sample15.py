b68daa34e19c352a15329448b8621ab69893a521
Liberbell <liberbell@gmail.com>
2021-02-09 08:47:04 +0900

    sample155

--- sample15.py
+++ sample15.py
@@ -1,7 +1,7 @@
 def debug(function):
     def _debug(*args, **kwargs):
         result = function(*args, **kwargs)
-        print(function, __name__, args, kwargs, result)
+        print(function.__name__, args, kwargs, result)
         return result
     return _debug
 
