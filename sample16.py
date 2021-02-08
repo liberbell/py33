04428ed69fd31ea580a8742739e8cf59777f5a83
Liberbell <liberbell@gmail.com>
2021-02-09 08:59:28 +0900

    sample164

--- sample16.py
+++ sample16.py
@@ -6,5 +6,12 @@ def make_squares(n):
 
 squares = make_squares(10)
 print(squares)
+
+
+def make_squares2(n):
+    for i in range(n, n+1):
+        yield i**2
+
+squares = make_squares2(10)
 for i in squares:
     print(i)
