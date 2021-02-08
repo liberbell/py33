2fcc0e0de83f437cd8addd0d9ab2ad05a56adaa5
Liberbell <liberbell@gmail.com>
2021-02-08 21:46:49 +0900

    sample144

--- sample14.py
+++ sample14.py
@@ -3,5 +3,11 @@ def create_int_list(numbers=[]):
         numbers.append(i)
     return numbers
 
-numbers = create_int_list([1,2,3])
+numbers = create_int_list()
 print(numbers)
+
+numbers2 = create_int_list(])
+print(numbers2)
+
+numbers3 = create_int_list()
+print(numbers3)
