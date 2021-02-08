cb6a3d9944be7877faebffb089af6bca0c36f6b3
Liberbell <liberbell@gmail.com>
2021-02-08 21:49:44 +0900

    sample146

--- sample14.py
+++ sample14.py
@@ -11,3 +11,19 @@ print(numbers2)
 
 numbers3 = create_int_list()
 print(numbers3)
+
+def create_int_list2(numbers=None):
+    if numbers is None:
+        numbers = []
+    for i in range(10):
+        numbers.append(i)
+    return numbers
+
+numbers = create_int_list2()
+print(numbers)
+
+numbers2 = create_int_list2()
+print(numbers2)
+
+numbers3 = create_int_list2()
+print(numbers3)
