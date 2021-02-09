266f1fda5971ba1cc6a9552688c2d51099dec377
Liberbell <liberbell@gmail.com>
2021-02-10 08:54:06 +0900

    sample173

--- sample17.py
+++ sample17.py
@@ -1,6 +1,11 @@
 def func():
     pass
 
+def my_sort(string):
+    return string[-1]
+
 my_list = ['python', 'django', 'tkinter', 'requests', 'kivy']
 my_list.sort()
 print(my_list)
+
+print(my_sort(my_list))
