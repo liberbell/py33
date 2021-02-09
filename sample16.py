c60d172460a1cd716ad5fe5f3ff7a2a7875dff9e
Liberbell <liberbell@gmail.com>
2021-02-10 08:44:31 +0900

    sample161

--- sample16.py
+++ sample16.py
@@ -1,17 +1,17 @@
-def make_squares(n):
-    numbers = []
-    for i in range(n, n+1):
-        numbers.append(i**2)
-    return numbers
+# def make_squares(n):
+#     numbers = []
+#     for i in range(n, n+1):
+#         numbers.append(i**2)
+#     return numbers
 
-squares = make_squares(10)
-print(squares)
+# squares = make_squares(10)
+# print(squares)
 
 
-def make_squares2(n):
-    for i in range(n, n+1):
-        yield i**2
+# def make_squares2(n):
+#     for i in range(n, n+1):
+#         yield i**2
 
-squares = make_squares2(10)
-for i in squares:
-    print(i)
+# squares = make_squares2(10)
+# for i in squares:
+#     print(i)
