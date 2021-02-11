f83224d6e56d6a100013ca366382d99f20c5f50d
Liberbell <liberbell@gmail.com>
2021-02-11 13:51:21 +0900

    sample187

--- sample18.py
+++ sample18.py
@@ -14,21 +14,26 @@
 # Bob_score = get_score('Bob')
 # print(Bob_score)
 
-members = {}
+# members = {}
 
-def add_score(name, subject, point):
-    student = members.setdefault(name, {})
-    student[subject] = point
+# def add_score(name, subject, point):
+#     student = members.setdefault(name, {})
+#     student[subject] = point
 
-def get_score(name, subject):
-    student = members.get(name)
-    if not student:
-        return 'nobody'
-    point = student.get(subject)
-    if not point:
-        return 'not yet the subject'
-    return point
+# def get_score(name, subject):
+#     student = members.get(name)
+#     if not student:
+#         return 'nobody'
+#     point = student.get(subject)
+#     if not point:
+#         return 'not yet the subject'
+#     return point
 
-add_score('Bob', 'science', 88)
-Bob_science = get_score('Bob', 'math')
-print(Bob_science)
+# add_score('Bob', 'science', 88)
+# Bob_science = get_score('Bob', 'math')
+# print(Bob_science)
+
+class Student:
+    pass
+
+a = Student()
