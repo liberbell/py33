babea354947437571b5d3333312e23373031a7b5
Liberbell <liberbell@gmail.com>
2021-02-11 13:44:28 +0900

    sample185

--- sample18.py
+++ sample18.py
@@ -1,15 +1,20 @@
-print(type(1))
-print(type('char'))
-print(type([1]))
+# print(type(1))
+# print(type('char'))
+# print(type([1]))
+
+# members = {}
+
+# def add_score(name, point):
+#     members[name] = point
+
+# def get_score(name):
+#     return members.get(name, 'nobody')
+
+# add_score('Bob', 50)
+# Bob_score = get_score('Bob')
+# print(Bob_score)
 
 members = {}
 
-def add_score(name, point):
-    members[name] = point
-
-def get_score(name):
-    return members.get(name, 'nobody')
-
-add_score('Bob', 50)
-Bob_score = get_score('Bob')
-print(Bob_score)
+def add_score(name, subject, point):
+    student = members.setdefault(name, {})
