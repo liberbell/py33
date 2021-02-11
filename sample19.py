501792f1fb9717e9246bf5bb110c6db7440dc56a
Liberbell <liberbell@gmail.com>
2021-02-12 08:48:56 +0900

    sample193

--- sample19.py
+++ sample19.py
@@ -2,5 +2,11 @@ class Character:
     def __init__(self, name):
         self.name = name
 
+class Monster(Character):
+    pass
+
 char_a = Character('CHAR A')
 print(char_a.name)
+
+monster_a = Monster('Nonster A')
+print(monster_a.name)
