17570b3ce73844ee385a7c666be0e0aa2176f940
Liberbell <liberbell@gmail.com>
2021-02-13 14:04:13 +0900

    sample216

--- sample21.py
+++ sample21.py
@@ -1,6 +1,20 @@
+# class A:
+#     def __init__(self):
+#         self.__value = 1
+
+# a = A()
+# print(a._A__value)
+
 class A:
     def __init__(self):
-        self.__value = 1
+        self.value = 1
 
-a = A()
-print(a._A__value)
+class B(A):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+class C(B):
+    def __init__(self, name):
+        super().__init__(name)
+        self.value = 10
