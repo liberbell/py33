d42a76da3d8fba97f0f4a09ffd8f0cb77cba0406
Liberbell <liberbell@gmail.com>
2021-02-14 13:38:49 +0900

    sample236

--- sample23.py
+++ sample23.py
@@ -1,21 +1,28 @@
+# class A:
+#     def __init__(self):
+#         self.count = 1
+
+#     def __len__(self):
+#         return 10
+
+#     def __iter__(self):
+#         return self
+
+#     def __next__(self):
+#         current = self.count
+#         if current > 10:
+#             raise StopIteration
+#         self.count += 1
+#         return current
+
+# a = A()
+# # print(len(a)
+# for count in a:
+#     print(count)
+
 class A:
-    def __init__(self):
-        self.count = 1
-
-    def __len__(self):
-        return 10
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        current = self.count
-        if current > 10:
-            raise StopIteration
-        self.count += 1
-        return current
+    def __getitem__(self, key):
+        return key
 
 a = A()
-# print(len(a)
-for count in a:
-    print(count)
+print(a[0])
