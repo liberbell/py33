dc203cbe5d1f7755580a6276055afd943c95a1ad
Liberbell <liberbell@gmail.com>
2021-02-14 13:37:22 +0900

    sample234

--- sample23.py
+++ sample23.py
@@ -1,9 +1,19 @@
 class A:
     def __init__(self):
-        pass
+        self.count = 
 
     def __len__(self):
         return 10
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        current = self.count
+        if current > 10:
+            raise StopIteration
+        self.count += 1
+        return current
 
 a = A()
 print(len(a))
