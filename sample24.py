a1a65fe2beabbab47c09b6ee3f8d57c707df3be8
Liberbell <liberbell@gmail.com>
2021-02-15 08:59:14 +0900

    sample245

--- sample24.py
+++ sample24.py
@@ -9,6 +9,13 @@
 # str = 'abcde'
 # print(str('aa'))
 
-from calc import *
-result = add(10, 20)
-print(result)
+# from calc import *
+# result = add(10, 20)
+# print(result)
+
+def add (a, b):
+    return a + b
+
+if __name__ == '__main__':
+    result = add(1, 3)
+    print(result)
