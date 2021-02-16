ffa8e6215c2371cb4c49aed519607f5cde27e5cb
Liberbell <liberbell@gmail.com>
2021-02-16 09:10:41 +0900

    sample279

--- sample27.py
+++ sample27.py
@@ -1,6 +1,7 @@
 # with open('hello.txt', 'x', encoding='utf-8') as file:
 #     file.write('hello')
 
+file = None
 try:
     file = open('hello.txt', 'x', encoding='utf-8')
 except FileExistsError:
