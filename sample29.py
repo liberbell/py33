bbd765ae1235c344d92c4e34c2a5f51b8b18c14a
Liberbell <liberbell@gmail.com>
2021-02-17 09:01:48 +0900

    sample298

--- sample29.py
+++ sample29.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get('https://blog.narito.ninja/')
+res = requests.get('https://www.yahoo.co.jp')
 soup = BeautifulSoup(res.text, 'html.parser')
 for h1 in soup.find_all('h1'):
     print(h1.text)
