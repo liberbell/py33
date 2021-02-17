70415149eae83a367e49b7f18c780de663dbafad
Liberbell <liberbell@gmail.com>
2021-02-17 09:00:41 +0900

    sample296

--- sample29.py
+++ sample29.py
@@ -3,4 +3,5 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://blog.narito.ninja/')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup)
+for h2 in soup.find_all('h2'):
+    print(h2.text)
